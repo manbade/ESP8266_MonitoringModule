1b7404e5573957ef078736212325e4cde07fb041
Dmytro <razzner@gmail.com>
2016-10-28 02:20:27 +0300

    added delay_list()

--- WebCommon.cpp
+++ WebCommon.cpp
@@ -7,9 +7,13 @@ String renderParameterRow(String paramName, String paramId, String paramValue, b
     return String("<div class='input-group'><label class='input_label' for='" + paramId + "'>" + paramName + "</label><input type='" + (isPassword ? "password" : "text") + "' id='" + paramId + "' class='form-control' " + (isReadonly ? readonlyAttr : "") + " value='" + paramValue + "' /></div>");
 }
 
-String renderParameterList(String paramValue, String paramDesc, bool isSelected)
+String renderParameterList(String paramValue, String paramDesc)
 {
     return String("<option value='" + paramValue + "'>" + paramDesc + "</option>");
+}
+
+String delay_list() {
+   return String("<option value='60'>1 хвилина</option><option value='300'>5 хвилин</option><option value='600'>10 хвилин</option><option value='900'>15 хвилин</option><option value='1200'>20 хвилин</option><option value='1500'>25 хвилин</option><option value='1800'>30 хвилин</option><option value='2100'>35 хвилин</option><option value='2400'>40 хвилин</option><option value='2700'>45 хвилин</option><option value='3000'>50 хвилин</option><option value='3300'>55 хвилин</option><option value='3600'>1 година</option><option value='7200'>2 години</option><option value='10800'>3 години</option><option value='14400'>4 години</option><option value='18000'>5 годин</option><option value='21600'>6 годин</option>");
 }
 
 String renderTitle(String moduleName, String pageName)
